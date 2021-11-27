b43e96b5b9d958a8755bf788e5e4bee3fff5e1a0
AakashRaina <aakashraina9@gmail.com>
2021-11-27 15:57:17 +0530

    Routing completed

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -7,13 +7,15 @@ export default () => {
   const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current, {
+    const { onParentNavigate } = mount(ref.current, {
       // update container's history when subapp changes it's route
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
         if (pathname !== nextPathname) history.push(nextPathname);
       },
     });
+
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref}></div>;

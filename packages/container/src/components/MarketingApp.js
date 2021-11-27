284a2a48d83dfec4c4ec0539e4af3317aafc7556
AakashRaina <aakashraina9@gmail.com>
2021-11-27 15:45:56 +0530

    Sync child and container history

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,19 @@
 import React, { useRef, useEffect } from "react";
 import { mount } from "marketing/MarketingApp";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
+    mount(ref.current, {
+      // update container's history when subapp changes it's route
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) history.push(nextPathname);
+      },
+    });
   }, []);
 
   return <div ref={ref}></div>;

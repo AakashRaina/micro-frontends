e6f8106e1a780f5c9ae45d9eedd851ec827d56b7
AakashRaina <aakashraina9@gmail.com>
2021-11-28 23:11:46 +0530

    Adds components for Auth

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -9,15 +9,18 @@ const generateClassName = createGenerateClassName({
   productionPrefix: "au",
 });
 
-// import Landing from "./components/Landing";
-// import Pricing from "./components/Pricing";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 
 export default ({ history }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
-          <Switch></Switch>
+          <Switch>
+            <Route path='/auth/signin' component={Signin} />
+            <Route path='/auth/signup' component={Signup} />
+          </Switch>
         </Router>
       </StylesProvider>
     </div>

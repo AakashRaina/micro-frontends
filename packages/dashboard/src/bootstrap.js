c1b8c8721b6031fe5c7086c3453ffee90cbd1c4c
AakashRaina <aakashraina9@gmail.com>
2021-12-05 16:25:13 +0530

    Set up dashboard to run in isolation

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -1,36 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createApp } from "vue";
+import Dashboard from "./components/Dashboard.vue";
 
-import { createMemoryHistory, createBrowserHistory } from "history";
-
-import App from "./App";
-
-const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
-  const history =
-    defaultHistory ||
-    createMemoryHistory({
-      initialEntries: [initialPath],
-    });
-
-  if (onNavigate) history.listen(onNavigate);
-
-  ReactDOM.render(<App history={history} onSignIn={onSignIn} />, el);
-
-  return {
-    onParentNavigate({ pathname: nextPathname }) {
-      const { pathname } = history.location;
-      if (pathname !== nextPathname) history.push(nextPathname);
-    },
-  };
+const mount = (el) => {
+  const app = createApp(Dashboard);
+  app.mount(el);
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_auth-dev-root");
 
   if (devRoot) {
-    mount(devRoot, {
-      defaultHistory: createBrowserHistory(),
-    });
+    mount(devRoot);
   }
 }
 
